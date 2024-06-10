79c9af071e73822857c732c7ca1261416cc142a3
Daniel Frimu <frimudaniel17@gmail.com>
2024-06-10 18:36:20 +0000

    Let's get started!

---
+++ sections/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <header>
+      <div class="w-full max-w-[1200px] mx-auto flex justify-between items-center">
+        <h4>Hatzdaniel</h4>
+
+        <nav>
+          <ul class="flex gap-5">
+            <li>About</li>
+            <li>Services</li>
+            <li>Contacts</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
